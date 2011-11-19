a9b96df6f6b1eeb202aae2ff92ee2136f50fc3f3
pvela <prabhu.v@gmail.com>
2011-11-19 16:20:12 -0700

    adding testing files

---
+++ test.js
@@ -0,0 +1,12 @@
+var nexmo = require('./lib/nexmo');
+nexmo.sendTextMessage(<FROM_NUMBER>,<TO_NUMBER>,'testing');
+nexmo.sendMessage({from:<FROM_NUMBER>,to:<TO_NUMBER>,text:'testing'},consolelog);
+function consolelog (messageResponse) {
+        console.dir(messageResponse);
+}
+nexmo.checkBalance(consolelog);
+nexmo.getPricing('US',consolelog);
+nexmo.getNumbers(consolelog);
+nexmo.searchNumbers('US',consolelog);
+nexmo.searchNumbers('US',303,consolelog);
+nexmo.changePassword('nexmoapi',consolelog);
