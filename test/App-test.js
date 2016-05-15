df264454dac4eca9952085972b9b16c2ea34f210
leggetter <phil@leggetter.co.uk>
2016-05-15 16:07:33 +0100

    Simplify App API

--- test/App-test.js
+++ test/App-test.js
@@ -2,22 +2,22 @@ import App from '../lib/App';
 
 import NexmoStub from './NexmoStub';
 
-var appAPIs = [
-  'getApplications',
-  'createApplication',
-  'getApplication',
-  'updateApplication',
-  'deleteApplication'
-];
+var appAPIMapping = {
+  'getApplications':    'get',
+  'createApplication':  'create',
+  'getApplication':     'get|someAppId',
+  'updateApplication':  'update',
+  'deleteApplication':  'delete'
+};
 
 describe('App Object', function () {
   
   it('should implement all v1 APIs', function() {
-    NexmoStub.checkAllFunctionsAreDefined(appAPIs, App);
+    NexmoStub.checkAllFunctionsAreDefined(appAPIMapping, App);
   });
   
   it('should proxy the function call to the underlying `nexmo` object', function() {
-    NexmoStub.checkAllFunctionsAreCalled(appAPIs, App);
+    NexmoStub.checkAllFunctionsAreCalled(appAPIMapping, App);
   });
   
 });
