17a5029155d8d6d5ce7fade8c13411acfbc411c9
leggetter <phil@leggetter.co.uk>
2016-08-30 10:24:18 +0100

    Move HTTP functionality into HttpClient object Lots more refactoring to be done. But this is a reasonable starting point.

---
+++ test/HttpClient-test.js
@@ -0,0 +1,131 @@
+import sinon from 'sinon';
+import expect from 'expect.js';
+
+import https from 'https';
+import http from 'http';
+
+import HttpClient from '../lib/HttpClient';
+
+var fakeHttp = {request: function() {}};
+var fakeRequest = {
+  end: function(){},
+  on: function(){}
+};
+
+var defaultHeaders = {
+  "Content-Type": "application/x-www-form-urlencoded",
+  "accept": "application/json"
+};
+
+describe('HttpClient Object', function () {
+  
+  afterEach(function() {
+    fakeHttp.request.restore();
+  });
+  
+  it('should support GET requests over https', function() {
+    var mock = sinon.mock(fakeHttp);
+    mock.expects('request')
+      .once()
+      .withArgs({
+        headers: defaultHeaders,
+        host: "api.nexmo.com",
+        method: "GET",
+        path: "/api",
+        port: 443
+      })
+      .returns(fakeRequest);
+    
+    var client = new HttpClient({https:fakeHttp, port:443});
+    
+    client.request({host:'api.nexmo.com', path: '/api'}, 'GET', {some: 'data'});
+  });
+  
+  it('should support GET requests over http', function() {
+    var mock = sinon.mock(fakeHttp);
+    mock.expects('request')
+      .once()
+      .withArgs({
+        headers: defaultHeaders,
+        host: "api.nexmo.com",
+        method: "GET",
+        path: "/api",
+        port: 80
+      })
+      .returns(fakeRequest);
+    
+    var client = new HttpClient({http:fakeHttp, port:80});
+    
+    client.request({host:'api.nexmo.com', path: '/api'}, 'GET', {some: 'data'});
+  });
+  
+  it('should be possible to set the host', function() {
+    var mock = sinon.mock(fakeHttp);
+    mock.expects('request')
+      .once()
+      .withArgs({
+        headers: defaultHeaders,
+        host: "rest.nexmo.com",
+        method: "GET",
+        path: "/api",
+        port: 80
+      })
+      .returns(fakeRequest);
+    
+    var client = new HttpClient({http:fakeHttp, port:80});
+    
+    client.request({host:'rest.nexmo.com', path: '/api'}, 'GET', {some: 'data'});
+  });
+  
+  it('should be possible to set the path', function() {
+    var mock = sinon.mock(fakeHttp);
+    mock.expects('request')
+      .once()
+      .withArgs({
+        headers: defaultHeaders,
+        host: "api.nexmo.com",
+        method: "GET",
+        path: "/some_path",
+        port: 80
+      })
+      .returns(fakeRequest);
+    
+    var client = new HttpClient({http:fakeHttp, port:80});
+    
+    client.request({host:'api.nexmo.com', path: '/some_path'}, 'GET', {some: 'data'});
+  });
+  
+  it('should be possible to set the method', function() {
+    var mock = sinon.mock(fakeHttp);
+    mock.expects('request')
+      .once()
+      .withArgs({
+        headers: defaultHeaders,
+        host: "api.nexmo.com",
+        method: "POST",
+        path: "/api",
+        port: 443
+      })
+      .returns(fakeRequest);
+    
+    var client = new HttpClient({https:fakeHttp});
+    
+    client.request({host:'api.nexmo.com', path: '/api'}, 'POST', {some: 'data'});
+  });
+  
+  it('should log requests', function() {
+    var mock = sinon.mock(fakeHttp);
+    mock.expects('request').returns(fakeRequest);
+    
+    var logged = false;
+    var log = function() {
+      logged = true;
+    }
+    var client = new HttpClient({https:fakeHttp, log:log});
+    
+    client.request({host:'api.nexmo.com', path: '/api'}, 'GET', {some: 'data'});
+    
+    expect(logged).to.be(true);
+  });
+
+});
