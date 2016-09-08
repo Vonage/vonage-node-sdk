8eb07090f72faddebb7d65dac59eb1323fbd08fc
leggetter <phil@leggetter.co.uk>
2016-09-08 23:32:03 +0100

    Add StreamResource for nexmo.calls.stream
    * stream.start(callId, params, callback)
    * stream.stop(callId, callback)

--- test/CallResource-test.js
+++ test/CallResource-test.js
@@ -4,9 +4,12 @@ import sinonChai        from 'sinon-chai';
 
 chai.use(sinonChai);
 
+import ResourceTestHelper from './ResourceTestHelper';
+
 import querystring from 'querystring';
 
 import CallsResource from '../lib/CallsResource';
+import StreamResource from '../lib/StreamResource';
 import HttpClient from '../lib/HttpClient';
 import Credentials from '../lib/Credentials';
 
@@ -15,44 +18,6 @@ var creds = Credentials.parse({
   privateKey: __dirname + '/private-test.key'
 });
 var emptyCallback = () => {};
-
-function getRequestArgs(params, overrides = {}) {
-  var callsArgs = {
-    host: overrides.host       || 'api.nexmo.com',
-    path: overrides.path       || '/beta/calls',
-    method: overrides.method   || 'POST',
-    body: overrides.hasOwnProperty('body')? overrides.body : JSON.stringify(params),
-    headers: overrides.headers || {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '
-    }
-  };
-  
-  // Removed undefined properties
-  Object.keys(callsArgs).forEach(function(key) {
-    if(callsArgs[key] === undefined) {
-      delete callsArgs[key];
-    }
-  })
-  
-  return callsArgs;
-}
-
-function requestArgsMatch(params, requestOverrides) {
-  return function(actual) {
-    var expected = getRequestArgs(params, requestOverrides);
-    
-    var match = (
-      expected.host === actual.host &&
-      expected.path === actual.path &&
-      expected.method === actual.method &&
-      expected.body === actual.body &&
-      expected.headers['Content-Type'] === actual.headers['Content-Type'] &&
-      actual.headers['Authorization'].indexOf(expected.headers['Authorization']) == 0
-    );
-    return match;
-  }
-}
 
 describe('CallsResource', () => {
   
@@ -71,7 +36,7 @@ describe('CallsResource', () => {
     var params = {};
     calls.create(params, emptyCallback);
     
-    var expectedRequestArgs = requestArgsMatch(params);
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(params);
     expect(httpClientStub.request)
       .to.have.been.calledWith(
         sinon.match(expectedRequestArgs),
@@ -82,7 +47,7 @@ describe('CallsResource', () => {
   it('should get a collection of calls', () => {
     calls.get(emptyCallback);
     
-    var expectedRequestArgs = requestArgsMatch(null, {
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
       method: 'GET',
       body: undefined,
       path: `${CallsResource.PATH}`
@@ -99,7 +64,7 @@ describe('CallsResource', () => {
     const callId = '2342342-lkjhlkjh-32423';
     calls.get(callId, emptyCallback);
     
-    var expectedRequestArgs = requestArgsMatch(null, {
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
       method: 'GET',
       body: undefined,
       path: `${CallsResource.PATH}/${callId}`
@@ -118,7 +83,7 @@ describe('CallsResource', () => {
     var params = {action: 'hangup'};
     calls.update(callId, params, emptyCallback);
     
-    var expectedRequestArgs = requestArgsMatch(params, {
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(params, {
       method: 'PUT',
       path: `${CallsResource.PATH}/${callId}`
     });
@@ -128,6 +93,10 @@ describe('CallsResource', () => {
         sinon.match(expectedRequestArgs),
         emptyCallback
       );
+  });
+  
+  it('should expose a stream property', () => {
+    expect(calls.stream).to.be.an.instanceOf(StreamResource);
   });
   
 });
