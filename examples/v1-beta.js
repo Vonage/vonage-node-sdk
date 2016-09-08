1c24580ba893efef55d5ab9a9296d72c428d5751
leggetter <phil@leggetter.co.uk>
2016-09-08 02:12:55 +0100

    Add Voice.call example for new Nexmo Voice API

--- examples/v1-beta.js
+++ examples/v1-beta.js
@@ -7,6 +7,8 @@ var API_KEY = process.env.API_KEY || '';
 var API_SECRET = process.env.API_SECRET || '';
 var FROM_NUMBER = process.env.FROM_NUMBER || '';
 var TO_NUMBER = process.env.TO_NUMBER || '';
+var APP_ID = process.env.APP_ID || '';
+var PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 
 function logToConsole (err,messageResponse) {
   if (err) {
@@ -21,39 +23,60 @@ var Nexmo = require('../lib/Nexmo');
 
 var nexmo = new Nexmo({
     apiKey: API_KEY, 
-    apiSecret: API_SECRET
+    apiSecret: API_SECRET,
+    applicationId: APP_ID,
+    privateKey: PRIVATE_KEY
   },
   {debug: true}
 );
 
-console.log('Sending Text Message');
-nexmo.message.sendSms(
-  FROM_NUMBER,
-  TO_NUMBER, 
-  'testing', 
-  logToConsole
-);
+// console.log('Sending Text Message');
+// nexmo.message.sendSms(
+//   FROM_NUMBER,
+//   TO_NUMBER, 
+//   'testing', 
+//   logToConsole
+// );
+// 
+// console.log('Getting Basic Number Insight');
+// nexmo.numberInsight.get({level:'basic', number: TO_NUMBER}, logToConsole);
+// 
+// console.log('Checking Balance');
+// nexmo.account.checkBalance(logToConsole);
+// 
+// console.log('Getting Apps List');
+// nexmo.app.get({}, logToConsole);
+// 
+// var tempAppName = new Date().getTime(); 
+// console.log('Creating App', tempAppName);
+// 
+// nexmo.app.create(tempAppName, 'voice', 'http://requestb.in/s8yhpcs8', 'http://requestb.in/s8yhpcs8', null, function(err, resp) {
+//   logToConsole(resp);
+//   
+//   console.log('Updating App', tempAppName);
+//   nexmo.app.update(resp.id, tempAppName, 'voice', 'http://requestb.in/s8yhpcs8', 'http://requestb.in/s8yhpcs8', null, function(err, resp) {
+//     
+//     console.log('Deleting App', tempAppName);
+//     nexmo.app.delete(resp.id, logToConsole);
+//     
+//   });
+// });
 
-console.log('Getting Basic Number Insight');
-nexmo.numberInsight.get({level:'basic', number: TO_NUMBER}, logToConsole);
-
-console.log('Checking Balance');
-nexmo.account.checkBalance(logToConsole);
-
-console.log('Getting Apps List');
-nexmo.app.get({}, logToConsole);
-
-var tempAppName = new Date().getTime(); 
-console.log('Creating App', tempAppName);
-
-nexmo.app.create(tempAppName, 'voice', 'http://requestb.in/s8yhpcs8', 'http://requestb.in/s8yhpcs8', null, function(err, resp) {
-  logToConsole(resp);
-  
-  console.log('Updating App', tempAppName);
-  nexmo.app.update(resp.id, tempAppName, 'voice', 'http://requestb.in/s8yhpcs8', 'http://requestb.in/s8yhpcs8', null, function(err, resp) {
-    
-    console.log('Deleting App', tempAppName);
-    nexmo.app.delete(resp.id, logToConsole);
-    
-  });
-});
+nexmo.voice.call({
+  to: [{
+    type: 'phone',
+    number: TO_NUMBER
+  }],
+  from: {
+    type: 'phone',
+    number: FROM_NUMBER
+  },
+  answer_url: ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json']
+}, function(err, resp) {
+  if(err) {
+    console.error(err);
+  }
+  else {
+    console.log(resp)
+  }
+})
