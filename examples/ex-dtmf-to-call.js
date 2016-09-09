3e0ecb2aa0588f1fe9e81adabde1097b47f778b3
leggetter <phil@leggetter.co.uk>
2016-09-09 12:55:04 +0100

    DTMF example

---
+++ examples/ex-dtmf-to-call.js
@@ -0,0 +1,35 @@
+module.exports = function(callback, config) {
+  
+  var Promise = require('bluebird');
+  var SPACER = '\n****\n\n';
+
+  var Nexmo = require('../lib/Nexmo');
+
+  var nexmo = new Nexmo({
+      apiKey: config.API_KEY, 
+      apiSecret: config.API_SECRET,
+      applicationId: config.APP_ID,
+      privateKey: config.PRIVATE_KEY
+    },
+    {debug: config.DEBUG}
+  );
+  
+  var calls = Promise.promisifyAll(nexmo.calls);
+  var dtmf = Promise.promisifyAll(nexmo.calls.dtmf);
+  var callId = null;
+  nexmo.calls.getAsync()
+    .then(function(resp) {
+      callId = resp._embedded.calls[0].uuid;
+      return dtmf.sendAsync(
+          callId,
+          {digits: [1, 2, 3, 4]}
+        );
+    })
+    .then(function(resp) {
+      callback(null, resp);
+    })
+    .then(function(resp) {
+      callback(null, resp);
+    })
+    .catch(callback);
+};
