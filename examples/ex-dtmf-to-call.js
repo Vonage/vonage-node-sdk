5fb006733e2ead156b656526a80f7876eb1e52b3
leggetter <phil@leggetter.co.uk>
2016-09-09 22:38:55 +0100

    Using ngrok + express to enable examples that support web hooks

--- examples/ex-dtmf-to-call.js
+++ examples/ex-dtmf-to-call.js
@@ -1,10 +1,10 @@
 module.exports = function(callback, config) {
   
   var Promise = require('bluebird');
+  var ngrok = require('ngrok');
   var SPACER = '\n****\n\n';
-
+  
   var Nexmo = require('../lib/Nexmo');
-
   var nexmo = new Nexmo({
       apiKey: config.API_KEY, 
       apiSecret: config.API_SECRET,
@@ -13,20 +13,82 @@ module.exports = function(callback, config) {
     },
     {debug: config.DEBUG}
   );
-  
   var calls = Promise.promisifyAll(nexmo.calls);
   var dtmf = Promise.promisifyAll(nexmo.calls.dtmf);
+  
+  function randomPort() {
+    return Math.floor(Math.random()*(7000-3000+1)+3000);
+  }
+  
+  var app = require('express')();
+  app.set('port', (process.env.PORT || randomPort()));
+  app.use(require('body-parser').json());
+  
+  // Handle events
   var callId = null;
-  nexmo.calls.getAsync()
-    .then(function(resp) {
-      callId = resp._embedded.calls[0].uuid;
-      return dtmf.sendAsync(
-          callId,
-          {digits: '1234'}
-        );
-    })
-    .then(function(resp) {
-      callback(null, resp);
-    })
-    .catch(callback);
+  app.post('/', function(req, res) {
+    console.log('Request received', req.body);
+    // Hack: seeing two inbound webhook requests.
+    // Use callId to indicate this.
+    if(req.body.status === 'answered' && !callId) {
+      callId = req.body.uuid;
+      
+      console.log('Call answered with call_uuid', callId)
+      
+      setTimeout(function() {
+        sendDtmf(callId);
+      }, 5000);
+    }
+    
+    res.sendStatus(204);
+  });
+  
+  var server = app.listen(app.get('port'), makeCall);
+  
+  function makeCall() {    
+    console.log('Web server listening on port', app.get('port'));
+      
+    Promise.promisify(ngrok.connect)(app.get('port'))
+      .then(function(url) {
+        console.log('ngrok tunnel set up:', url);
+        
+        console.log('calling', config.TO_NUMBER);
+        return calls.createAsync({
+          to: [{
+            type: 'phone',
+            number: config.TO_NUMBER
+          }],
+          from: {
+            type: 'phone',
+            number: config.FROM_NUMBER
+          },
+          answer_url: ['https://nexmo-community.github.io/ncco-examples/conference.json'],
+          event_url: [url]
+        });
+      })
+      .then(function(res) {
+        console.log('call in progress', res);
+      })
+      .catch(callback);
+  }
+  
+  function sendDtmf(callId) {
+    dtmf.sendAsync(
+        callId,
+        {digits: '1234'}
+      )
+      .then(function(res) {
+        server.close();
+        ngrok.kill();
+        
+        callback(null, res);
+      })
+      .catch(function(err) {
+        if(server) server.close();
+        if(ngrok) ngrok.kill();
+        
+        callback(err);
+      });
+  }
+
 };
