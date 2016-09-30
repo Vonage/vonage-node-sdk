c3cf0f5ecec12f9206a2e442562c7d85d055b667
Cristiano Betta <cbetta@gmail.com>
2016-09-30 16:34:09 +0100

    Linted tests. Refactored tests to make liniting pass.
    * Use ESLint to lint all test files enforcing:
      * No trailing white space
      * Trailing semi colons
      * Camelcase variables and params
      * Unix linebreaks (no windows line breaks which will break things
      * Single quotes for regular string, backticks for interpolated strings
      * Warnings on console statements left in code
    * All tests still pass
    * Used Atom-Beautify gem package to fix indentation and restructure code to be more readable

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'node': true,
+        'mocha': true,
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-console': [
+            'warn'
+        ],
+        'no-trailing-spaces': [
+          'error'
+        ],
+        'camelcase': [
+          'error'
+        ]
+    }
+};
--- test/ResourceTestHelper.js
+++ test/ResourceTestHelper.js
@@ -1,42 +1,42 @@
 class ResourceTestHelper {
-  
-  static getRequestArgs(params, overrides = {}) {
-    var callsArgs = {
-      host: overrides.host       || 'api.nexmo.com',
-      path: overrides.path       || '/v1/calls',
-      method: overrides.method   || 'POST',
-      body: overrides.hasOwnProperty('body')? overrides.body : JSON.stringify(params),
-      headers: overrides.headers || {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer '
-      }
-    };
-    
-    // Removed undefined properties
-    Object.keys(callsArgs).forEach(function(key) {
-      if(callsArgs[key] === undefined) {
-        delete callsArgs[key];
-      }
-    });
-    
-    return callsArgs;
-  }
 
-  static requestArgsMatch(params, requestOverrides) {
-    return function(actual) {
-      var expected = ResourceTestHelper.getRequestArgs(params, requestOverrides);
-      
-      var match = (
-        expected.host === actual.host &&
-        expected.path === actual.path &&
-        expected.method === actual.method &&
-        expected.body === actual.body &&
-        expected.headers['Content-Type'] === actual.headers['Content-Type'] &&
-        actual.headers['Authorization'].indexOf(expected.headers['Authorization']) == 0
-      );
-      return match;
+    static getRequestArgs(params, overrides = {}) {
+        var callsArgs = {
+            host: overrides.host || 'api.nexmo.com',
+            path: overrides.path || '/v1/calls',
+            method: overrides.method || 'POST',
+            body: overrides.hasOwnProperty('body') ? overrides.body : JSON.stringify(params),
+            headers: overrides.headers || {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '
+            }
+        };
+
+        // Removed undefined properties
+        Object.keys(callsArgs).forEach(function(key) {
+            if (callsArgs[key] === undefined) {
+                delete callsArgs[key];
+            }
+        });
+
+        return callsArgs;
     }
-  }
+
+    static requestArgsMatch(params, requestOverrides) {
+        return function(actual) {
+            var expected = ResourceTestHelper.getRequestArgs(params, requestOverrides);
+
+            var match = (
+                expected.host === actual.host &&
+                expected.path === actual.path &&
+                expected.method === actual.method &&
+                expected.body === actual.body &&
+                expected.headers['Content-Type'] === actual.headers['Content-Type'] &&
+                actual.headers['Authorization'].indexOf(expected.headers['Authorization']) == 0
+            );
+            return match;
+        };
+    }
 }
 
 export default ResourceTestHelper;
