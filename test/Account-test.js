8d3b496ea65d3fb7ba4bb14d21b769eda9a6f2b5
Cristiano Betta <cbetta@gmail.com>
2016-09-30 16:34:09 +0100

    Linted tests. Refactored tests to make liniting pass.
    * Use ESLint to lint all test files enforcing:
      * No trailing white space
      * Trailing semi colons
      * Camelcase variables and params
      * Unix linebreaks (no windows line breaks which will break things
      * Single quotes for regular string, backticks for interpolated strings
      * Warnings on console statements left in code
    * All tests still pass
    * Used Atom-Beautify gem package to fix indentation and restructure code to be more readable

--- test/Account-test.js
+++ test/Account-test.js
@@ -3,20 +3,20 @@ import Account from '../lib/Account';
 import NexmoStub from './NexmoStub';
 
 var accountAPIs = {
-  'checkBalance': 'checkBalance',
-  'changePassword': 'updatePassword',
-  'changeMoCallbackUrl': 'updateSMSCallback',
-  'changeDrCallbackUrl': 'updateDeliveryReceiptCallback'
+    'checkBalance': 'checkBalance',
+    'changePassword': 'updatePassword',
+    'changeMoCallbackUrl': 'updateSMSCallback',
+    'changeDrCallbackUrl': 'updateDeliveryReceiptCallback'
 };
 
-describe('Account Object', function () {
-  
-  it('should implement all v1 APIs', function() {
-    NexmoStub.checkAllFunctionsAreDefined(accountAPIs, Account);
-  });
-  
-  it('should proxy the function call to the underlying `nexmo` object', function() {
-    NexmoStub.checkAllFunctionsAreCalled(accountAPIs, Account);
-  });
-  
+describe('Account Object', function() {
+
+    it('should implement all v1 APIs', function() {
+        NexmoStub.checkAllFunctionsAreDefined(accountAPIs, Account);
+    });
+
+    it('should proxy the function call to the underlying `nexmo` object', function() {
+        NexmoStub.checkAllFunctionsAreCalled(accountAPIs, Account);
+    });
+
 });
--- test/Verify-test.js
+++ test/Verify-test.js
@@ -3,20 +3,20 @@ import Verify from '../lib/Verify';
 import NexmoStub from './NexmoStub';
 
 var verifyAPIs = {
-  'verifyNumber': 'request',
-  'checkVerifyRequest': 'check',
-  'controlVerifyRequest': 'control',
-  'searchVerifyRequest': 'search'  
+    'verifyNumber': 'request',
+    'checkVerifyRequest': 'check',
+    'controlVerifyRequest': 'control',
+    'searchVerifyRequest': 'search'
 };
 
-describe('Verify Object', function () {
-  
-  it('should implement all v1 APIs', function() {
-    NexmoStub.checkAllFunctionsAreDefined(verifyAPIs, Verify);
-  });
-  
-  it('should proxy the function call to the underlying `nexmo` object', function() {
-    NexmoStub.checkAllFunctionsAreCalled(verifyAPIs, Verify);
-  });
-  
+describe('Verify Object', function() {
+
+    it('should implement all v1 APIs', function() {
+        NexmoStub.checkAllFunctionsAreDefined(verifyAPIs, Verify);
+    });
+
+    it('should proxy the function call to the underlying `nexmo` object', function() {
+        NexmoStub.checkAllFunctionsAreCalled(verifyAPIs, Verify);
+    });
+
 });
