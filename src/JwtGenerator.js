beb7c10b2de287a76f94cc67899d832f80290260
leggetter <phil@leggetter.co.uk>
2016-10-06 23:53:43 +0100

    Static Nexmo.generateJwt(privateKey, claims)
    JwtGenerator updated to take privateKey and claims
    where `claims` is a set of name/value pairs that
    are converted to claims on the generated JWT.

--- src/JwtGenerator.js
+++ src/JwtGenerator.js
@@ -3,21 +3,27 @@ import jwt from 'jsonwebtoken';
 
 class JwtGenerator {
 	/**
-	 * Generate a JSON Web Token (JWT) for the application with the given
-	 * `applicationId`.
+	 * Generate a JSON Web Token (JWT).
+	 *
+	 * @param {Buffer} cert - the private key certificate to be used when signing
+	 *		the claims.
+	 * @param {Object} claims - additional claims to include within the generated
+	 * 		JWT.
 	 *
 	 * @returns {String} the generated token
 	 */
-	generate(cert, applicationId, issuedAt = Date.now(), jwtIdClaim = uuid.v1()) {
+	generate(cert, claims = {}) {
 		if(!cert instanceof Buffer) {
 			throw new Error('cert must be of type Buffer');
 		}
 		
 		var toSign = {
-			'iat': issuedAt,
-			'application_id': applicationId,
-			'jti': jwtIdClaim
+			'iat': claims.issuedAt || (Date.now()/1000),
+			'jti': claims.jti || uuid.v1()
 		};
+		Object.keys(claims).forEach((key) => {
+			toSign[key] = claims[key];
+		});
 		
 		var token = jwt.sign(toSign, cert, {algorithm: 'RS256'});
 		return token;
