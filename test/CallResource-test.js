3a05292b33d46ff7978a2d13d15983a4cecc2649
Mike Elsmore <mike.elsmore@uk.ibm.com>
2017-02-18 23:17:09 +0000

    Stop the ESLint errors so I can actually start playing

--- test/CallResource-test.js
+++ test/CallResource-test.js
@@ -16,124 +16,124 @@ import HttpClient from '../lib/HttpClient';
 import Credentials from '../lib/Credentials';
 
 var creds = Credentials.parse({
-    applicationId: 'some-id',
-    privateKey: __dirname + '/private-test.key'
+  applicationId: 'some-id',
+  privateKey: __dirname + '/private-test.key'
 });
 var emptyCallback = () => {};
 
 describe('CallsResource', () => {
 
-    var httpClientStub = null;
-    var calls = null;
+  var httpClientStub = null;
+  var calls = null;
 
-    beforeEach(() => {
-        httpClientStub = sinon.createStubInstance(HttpClient);
-        var options = {
-            httpClient: httpClientStub
-        };
-        calls = new CallsResource(creds, options);
+  beforeEach(() => {
+    httpClientStub = sinon.createStubInstance(HttpClient);
+    var options = {
+      httpClient: httpClientStub
+    };
+    calls = new CallsResource(creds, options);
+  });
+
+  it('should allow a call to be created', () => {
+    var params = {};
+    calls.create(params, emptyCallback);
+
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(params);
+    expect(httpClientStub.request)
+      .to.have.been.calledWith(
+        sinon.match(expectedRequestArgs),
+        emptyCallback
+      );
+  });
+
+  it('should throw an error if no query is provided', () => {
+    var expectThrow = function() {
+      calls.get();
+    };
+
+    expect(expectThrow).to.throw(Error);
+  });
+
+  it('should get a collection of calls', () => {
+    calls.get({}, emptyCallback);
+
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
+      method: 'GET',
+      body: undefined,
+      path: `${CallsResource.PATH}`
     });
 
-    it('should allow a call to be created', () => {
-        var params = {};
-        calls.create(params, emptyCallback);
+    expect(httpClientStub.request)
+      .to.have.been.calledWith(
+        sinon.match(expectedRequestArgs),
+        emptyCallback
+      );
+  });
 
-        var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(params);
-        expect(httpClientStub.request)
-            .to.have.been.calledWith(
-                sinon.match(expectedRequestArgs),
-                emptyCallback
-            );
+  it('should get a single of call using a call ID', () => {
+    const callId = '2342342-lkjhlkjh-32423';
+    calls.get(callId, emptyCallback);
+
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
+      method: 'GET',
+      body: undefined,
+      path: `${CallsResource.PATH}/${callId}`
     });
 
-    it('should throw an error if no query is provided', () => {
-        var expectThrow = function() {
-            calls.get();
-        };
+    expect(httpClientStub.request)
+      .to.have.been.calledWith(
+        sinon.match(expectedRequestArgs),
+        emptyCallback
+      );
+  });
 
-        expect(expectThrow).to.throw(Error);
+  it('should get a allow calls to be queried by filter', () => {
+    calls.get({
+      status: 'answered'
+    }, emptyCallback);
+
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
+      method: 'GET',
+      body: undefined,
+      path: `${CallsResource.PATH}?status=answered`
     });
 
-    it('should get a collection of calls', () => {
-        calls.get({}, emptyCallback);
+    expect(httpClientStub.request)
+      .to.have.been.calledWith(
+        sinon.match(expectedRequestArgs),
+        emptyCallback
+      );
+  });
 
-        var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
-            method: 'GET',
-            body: undefined,
-            path: `${CallsResource.PATH}`
-        });
+  it('should allow a call to be updated', () => {
+    const callId = '2342342-lkjhlkjh-32423';
+    var params = {
+      action: 'hangup'
+    };
+    calls.update(callId, params, emptyCallback);
 
-        expect(httpClientStub.request)
-            .to.have.been.calledWith(
-                sinon.match(expectedRequestArgs),
-                emptyCallback
-            );
+    var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(params, {
+      method: 'PUT',
+      path: `${CallsResource.PATH}/${callId}`
     });
 
-    it('should get a single of call using a call ID', () => {
-        const callId = '2342342-lkjhlkjh-32423';
-        calls.get(callId, emptyCallback);
+    expect(httpClientStub.request)
+      .to.have.been.calledWith(
+        sinon.match(expectedRequestArgs),
+        emptyCallback
+      );
+  });
 
-        var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
-            method: 'GET',
-            body: undefined,
-            path: `${CallsResource.PATH}/${callId}`
-        });
+  it('should expose a stream property', () => {
+    expect(calls.stream).to.be.an.instanceOf(StreamResource);
+  });
 
-        expect(httpClientStub.request)
-            .to.have.been.calledWith(
-                sinon.match(expectedRequestArgs),
-                emptyCallback
-            );
-    });
+  it('should expose a talk property', () => {
+    expect(calls.talk).to.be.an.instanceOf(TalkResource);
+  });
 
-    it('should get a allow calls to be queried by filter', () => {
-        calls.get({
-            status: 'answered'
-        }, emptyCallback);
-
-        var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(null, {
-            method: 'GET',
-            body: undefined,
-            path: `${CallsResource.PATH}?status=answered`
-        });
-
-        expect(httpClientStub.request)
-            .to.have.been.calledWith(
-                sinon.match(expectedRequestArgs),
-                emptyCallback
-            );
-    });
-
-    it('should allow a call to be updated', () => {
-        const callId = '2342342-lkjhlkjh-32423';
-        var params = {
-            action: 'hangup'
-        };
-        calls.update(callId, params, emptyCallback);
-
-        var expectedRequestArgs = ResourceTestHelper.requestArgsMatch(params, {
-            method: 'PUT',
-            path: `${CallsResource.PATH}/${callId}`
-        });
-
-        expect(httpClientStub.request)
-            .to.have.been.calledWith(
-                sinon.match(expectedRequestArgs),
-                emptyCallback
-            );
-    });
-
-    it('should expose a stream property', () => {
-        expect(calls.stream).to.be.an.instanceOf(StreamResource);
-    });
-
-    it('should expose a talk property', () => {
-        expect(calls.talk).to.be.an.instanceOf(TalkResource);
-    });
-
-    it('should expose a dtmf property', () => {
-        expect(calls.dtmf).to.be.an.instanceOf(DtmfResource);
-    });
+  it('should expose a dtmf property', () => {
+    expect(calls.dtmf).to.be.an.instanceOf(DtmfResource);
+  });
 
 });
