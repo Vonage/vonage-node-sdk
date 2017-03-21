429708aa559efed184a4f5398a4157ae5e01e0e9
leggetter <phil@leggetter.co.uk>
2017-03-21 11:17:37 +0000

    Example for making multiple calls

--- examples/config.js
+++ examples/config.js
@@ -4,6 +4,7 @@ var config = {
   API_KEY: process.env.API_KEY || '',
   API_SECRET: process.env.API_SECRET || '',
   FROM_NUMBER: process.env.FROM_NUMBER || '',
+  ALT_TO_NUMBER: process.env.ALT_TO_NUMBER || '',
   TO_NUMBER: process.env.TO_NUMBER || '',
   APP_ID: process.env.APP_ID || '',
   PRIVATE_KEY: process.env.PRIVATE_KEY || '',
