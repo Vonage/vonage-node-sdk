63d6c2f93711b224d9db7ef9d4a39bd0e5667714
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-15 15:06:45 +0530

    feat: add number insight typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -10,7 +10,7 @@
  *       CheckResponse -> VerifyCheckResponse
  */
 
-declare module "@vonage/server-sdk" {
+declare module '@vonage/server-sdk' {
     /* general */
     export interface CredentialsObject {
         apiKey: string;
@@ -20,8 +20,8 @@ declare module "@vonage/server-sdk" {
     }
 
     export interface VonageApiError {
-        body: { [key: string]: any };
-        headers?: { [key: string]: any };
+      body: { [key: string]: any };
+      headers?: { [key: string]: any };
     }
 
     export interface ParserError {
@@ -33,83 +33,80 @@ declare module "@vonage/server-sdk" {
 
     /* message API */
     export enum MessageRequestResponseStatusCode {
-        Success = "0",
-        Throttled = "1",
-        MissingParameters = "2",
-        InvalidParameters = "3",
-        InvalidCredentials = "4",
-        InternalError = "5",
-        InvalidMessage = "6",
-        NumberBarred = "7",
-        PartnerAccountBarred = "8",
-        PartnerQuotaViolation = "9",
-        TooManyExistingBinds = "10",
-        AccountNotEnabledForHTTP = "11",
-        MessageTooLong = "12",
-        InvalidSignature = "14",
-        InvalidSenderAddress = "15",
-        InvalidNetworkCode = "22",
-        InvalidCallbackURL = "23",
-        NonWhitelistedDestination = "29",
-        SignatureAndAPISecretDisallowed = "32",
-        NumberDeActivated = "33"
+        Success = '0',
+        Throttled = '1',
+        MissingParameters = '2',
+        InvalidParameters = '3',
+        InvalidCredentials = '4',
+        InternalError = '5',
+        InvalidMessage = '6',
+        NumberBarred = '7',
+        PartnerAccountBarred = '8',
+        PartnerQuotaViolation = '9',
+        TooManyExistingBinds = '10',
+        AccountNotEnabledForHTTP = '11',
+        MessageTooLong = '12',
+        InvalidSignature = '14',
+        InvalidSenderAddress = '15',
+        InvalidNetworkCode = '22',
+        InvalidCallbackURL = '23',
+        NonWhitelistedDestination = '29',
+        SignatureAndAPISecretDisallowed = '32',
+        NumberDeActivated = '33',
     }
 
     export interface MessageRequestResponseSuccess {
-        to: string;
-        "message-id": string;
-        status: MessageRequestResponseStatusCode;
-        "remaining-balance": string;
-        "message-price": string;
-        network: string;
-        "account-ref": string;
+        to: string,
+        'message-id': string,
+        status: MessageRequestResponseStatusCode,
+        'remaining-balance': string,
+        'message-price': string,
+        'network': string,
+        'account-ref': string,
     }
 
     export interface MessageError {
         status: MessageRequestResponseStatusCode;
-        "error-text": string;
+        'error-text': string;
     }
-
+    
     export interface MessageRequestResponse {
-        "message-count": number;
-        messages: (MessageRequestResponseSuccess | MessageError)[];
+      'message-count': number;
+      messages: (MessageRequestResponseSuccess | MessageError)[];
     }
-
+    
     export interface SendSmsOptions {
         from: string;
         to: string;
         text?: string;
         sig?: string;
         ttl?: number;
-        "status-report-req"?: boolean;
+        'status-report-req'?: boolean;
         callback?: string;
-        "message-class"?: number;
+        'message-class'?: number;
         type?: string;
         vcard?: string | any;
         vcal?: string | any;
         body?: string;
         udh?: string;
-        "protocol-id"?: number;
+        'protocol-id'?: number;
         title?: string;
         url?: string;
         validity?: string;
-        "client-ref"?: string;
-        "account-ref"?: string;
+        'client-ref'?: string;
+        'account-ref'?: string;
     }
-
+    
     export type SendSms = (
-        sender: string,
-        recipient: string,
-        message: string,
-        opts: Partial<SendSmsOptions>,
-        callback: (err: MessageError, data: MessageRequestResponse) => void
+      sender: string,
+      recipient: string,
+      message: string,
+      opts: Partial<SendSmsOptions>,
+      callback: (err: MessageError, data: MessageRequestResponse) => void
     ) => void;
-
+    
     export class Message {
-        constructor(
-            credentials: CredentialsObject,
-            options: { [key: string]: any }
-        );
+        constructor(credentials: CredentialsObject, options: { [key: string]: any });
         sendSms: SendSms;
 
         /**
@@ -122,7 +119,7 @@ declare module "@vonage/server-sdk" {
         shortcodeMarketing: any;
         search: any;
         searchRejections: any;
-
+      
         __proto__: any;
         [key: string]: any;
     }
@@ -167,7 +164,7 @@ declare module "@vonage/server-sdk" {
         WrongVerificationCode = "11",
         TooManyRequests = "12",
         NoMoreEvents = "13",
-        NoRequestFound = "14"
+        NoRequestFound = "14",
     }
 
     export interface ControlObject {
@@ -182,7 +179,7 @@ declare module "@vonage/server-sdk" {
 
     export enum ControlResponseStatusCode {
         Success = "0",
-        CancelOrTriggerNextEvent = "19"
+        CancelOrTriggerNextEvent = "19",
     }
 
     export interface CheckObject {
@@ -200,139 +197,12 @@ declare module "@vonage/server-sdk" {
     }
 
     export class Verify {
-        constructor(
-            credentials: CredentialsObject,
-            options: { [key: string]: any }
-        );
-        request(
-            request: RequestObject,
-            callback: (err: VerifyError, data: RequestResponse) => void
-        ): void;
-        control(
-            request: ControlObject,
-            callback: (err: VerifyError, data: ControlResponse) => void
-        ): void;
-        check(
-            request: CheckObject,
-            callback: (err: VerifyError, data: CheckResponse) => void
-        ): void;
+        constructor(credentials: CredentialsObject, options: { [key: string]: any });
+        request(request: RequestObject, callback: (err: VerifyError, data: RequestResponse) => void): void;
+        control(request: ControlObject, callback: (err: VerifyError, data: ControlResponse) => void): void;
+        check(request: CheckObject, callback: (err: VerifyError, data: CheckResponse) => void): void;
         __proto__: any;
         [key: string]: any;
-    }
-
-    /* number-insight API */
-    export interface NumberInsightError extends VonageApiError {
-        status: NumberInsightResponseStatusCode | number;
-        error_text: string;
-        [key: string]: any;
-    }
-
-    export interface NumberInsightObject {
-        level: NumberInsightLevel;
-        number: string;
-        country?: string;
-        cnam?: boolean;
-        callback?: string;
-        ip?: string;
-    }
-
-    export interface NumberInsightResponse {
-        status: NumberInsightResponseStatusCode | number;
-        status_message: string;
-        request_id: string;
-        international_format_number: string;
-        national_format_number: string;
-        country_code: string;
-        country_code_iso3: string;
-        country_name: string;
-        country_prefix: string;
-        request_price?: string;
-        refund_price?: string;
-        remaining_balance?: string;
-        current_carrier?: {
-            network_code?: string;
-            name?: string;
-            country?: string;
-            network_type?: string;
-        };
-        original_carrier?: {
-            network_code?: string;
-            name?: string;
-            country?: string;
-            network_type?: string;
-        };
-        ported?: string;
-        roaming?: {
-            status?: string;
-            roaming_country_code?: string;
-            roaming_network_code?: string;
-            roaming_network_name?: string;
-        };
-        caller_identity?: {
-            caller_type?: string;
-            caller_name?: string;
-            first_name?: string;
-            last_name?: string;
-        };
-        caller_name?: string;
-        first_name?: string;
-        last_name?: string;
-        caller_type?: NumberInsightCallerType;
-        lookup_outcome?: NumberInsightLookupCode;
-        lookup_outcome_message?: string;
-        valid_number?: string;
-        reachable?: string;
-        error_text?: string;
-    }
-
-    export interface NumberInsightWebhookRequest
-        extends NumberInsightResponse {}
-
-    export enum NumberInsightLookupCode {
-        Success = 0,
-        PartialSuccess = 1,
-        Failed = 2
-    }
-
-    export enum NumberInsightLevel {
-        Basic = "basic",
-        Standard = "standard",
-        AdvancedAsync = "advancedAsync",
-        AdvancedSync = "advancedSync"
-    }
-
-    export enum NumberInsightCallerType {
-        Business = "business",
-        Consumer = "consumer",
-        Unknown = "unknown"
-    }
-
-    export enum NumberInsightResponseStatusCode {
-        Success = 0,
-        Busy = 1,
-        InvalidRequest = 3,
-        InvalidCredentials = 4,
-        InternalError = 5,
-        PartnerQuotaExceeded = 9,
-        FacilityNotAllowed = 19,
-        LiveMobileLookupNotReturned1 = 43,
-        LiveMobileLookupNotReturned2 = 44,
-        LiveMobileLookupNotReturned3 = 45,
-        RequestUnparseable = 999
-    }
-
-    export class NumberInsight {
-        constructor(
-            credentials: CredentialsObject,
-            options: { [key: string]: any }
-        );
-        get(
-            options: NumberInsightObject,
-            callback: (
-                err: NumberInsightError,
-                data: NumberInsightResponse
-            ) => void
-        ): void;
     }
 
     /* Media API */
@@ -404,15 +274,195 @@ declare module "@vonage/server-sdk" {
         [key: string]: any;
     }
 
-    /* Vonage */
-    export default class Vonage {
+    /* number API */
+    export interface INumberResponse {
+        'error-code': string;
+        'error-code-label': string;
+    }
+
+    export interface INumber {
+        country: string;
+        msisdn: string;
+        moHttpUrl: string;
+        type: string;
+        cost: string;
+        features: string[];
+        messagesCallbackType: string;
+        messagesCallbackValue: string;
+        voiceCallbackType: string;
+        voiceCallbackValue: string;
+    }
+
+    export interface getNumberResponse {
+        count: number;
+        numbers: Omit<Required<INumber>, 'cost'>[];
+    }
+
+    export interface searchNumberResponse {
+        count: number;
+        numbers: Pick<Required<INumber>, 'country' | 'msisdn' | 'type' | 'cost' | 'features'>[];
+    }
+
+    export class Number {
         constructor(
             credentials: CredentialsObject,
-            options?: { [key: string]: any }
+            options: { [key: string]: any }
         );
+        getPricing(): void;
+        getPhonePricing(): void;
+        get(
+            options: { [key: string]: any } | ((error?: INumberResponse, response?: getNumberResponse) => void),
+            callback?: (error?: INumberResponse, response?: getNumberResponse) => void
+        ): void;
+        search(
+            countryCode?: string,
+            pattern?: unknown,
+            callback?: (error?: INumberResponse, response?: searchNumberResponse) => void
+        ): void;
+        buy(
+            countryCode?: string,
+            msisdn?: string,
+            targetApiKey?: string | ((error?: INumberResponse, response?: INumberResponse) => void),
+            callback?: (error?: INumberResponse, response?: INumberResponse) => void
+        ): void;
+        cancel(
+            countryCode?: string,
+            msisdn?: string,
+            targetApiKey?: string | ((error?: INumberResponse, response?: INumberResponse) => void),
+            callback?: (error?: INumberResponse, response?: INumberResponse) => void
+        ): void;
+        update(
+            countryCode?: string,
+            msisdn?: string,
+            params?: string | ((error?: INumberResponse, response?: INumberResponse) => void),
+            callback?: (error?: INumberResponse, response?: INumberResponse) => void
+        ): void;
+        __proto__: any;
+        [key: string]: any;
+    }
+
+    /* number-insight API */
+    export interface NumberInsightError extends VonageApiError {
+        status: NumberInsightResponseStatusCode | number;
+        error_text: string;
+        [key: string]: any;
+    }
+
+    export interface NumberInsightObject {
+        level: NumberInsightLevel;
+        number: string;
+        country?: string;
+        cnam?: boolean;
+        callback?: string;
+        ip?: string;
+    }
+
+    export interface NumberInsightResponse {
+        status: NumberInsightResponseStatusCode | number;
+        status_message: string;
+        request_id: string;
+        international_format_number: string;
+        national_format_number: string;
+        country_code: string;
+        country_code_iso3: string;
+        country_name: string;
+        country_prefix: string;
+        request_price?: string;
+        refund_price?: string;
+        remaining_balance?: string;
+        current_carrier?: {
+            network_code?: string;
+            name?: string;
+            country?: string;
+            network_type?: string;
+        };
+        original_carrier?: {
+            network_code?: string;
+            name?: string;
+            country?: string;
+            network_type?: string;
+        };
+        ported?: string;
+        roaming?: {
+            status?: string;
+            roaming_country_code?: string;
+            roaming_network_code?: string;
+            roaming_network_name?: string;
+        };
+        caller_identity?: {
+            caller_type?: string;
+            caller_name?: string;
+            first_name?: string;
+            last_name?: string;
+        };
+        caller_name?: string;
+        first_name?: string;
+        last_name?: string;
+        caller_type?: NumberInsightCallerType;
+        lookup_outcome?: NumberInsightLookupCode;
+        lookup_outcome_message?: string;
+        valid_number?: string;
+        reachable?: string;
+        error_text?: string;
+    }
+
+    export interface NumberInsightWebhookRequest
+        extends NumberInsightResponse { }
+
+    export enum NumberInsightLookupCode {
+        Success = 0,
+        PartialSuccess = 1,
+        Failed = 2
+    }
+
+    export enum NumberInsightLevel {
+        Basic = "basic",
+        Standard = "standard",
+        AdvancedAsync = "advancedAsync",
+        AdvancedSync = "advancedSync"
+    }
+
+    export enum NumberInsightCallerType {
+        Business = "business",
+        Consumer = "consumer",
+        Unknown = "unknown"
+    }
+
+    export enum NumberInsightResponseStatusCode {
+        Success = 0,
+        Busy = 1,
+        InvalidRequest = 3,
+        InvalidCredentials = 4,
+        InternalError = 5,
+        PartnerQuotaExceeded = 9,
+        FacilityNotAllowed = 19,
+        LiveMobileLookupNotReturned1 = 43,
+        LiveMobileLookupNotReturned2 = 44,
+        LiveMobileLookupNotReturned3 = 45,
+        RequestUnparseable = 999
+    }
+
+    export class NumberInsight {
+        constructor(
+            credentials: CredentialsObject,
+            options: { [key: string]: any }
+        );
+        get(
+            options: NumberInsightObject,
+            callback: (
+                err: NumberInsightError,
+                data: NumberInsightResponse
+            ) => void
+        ): void;
+    }
+
+    /* Vonage */
+    export default class Vonage {
+        constructor(credentials: CredentialsObject, options?: { [key: string]: any });
         public readonly verify: Verify;
         public readonly message: Message;
-        public readonly numberInsight: NumberInsight;
         public readonly media: Media;
+        public readonly number: Number;
+        public readonly numberInsight: NumberInsight;
     }
 }
