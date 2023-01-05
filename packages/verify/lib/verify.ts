7fb20f209a48a7da373313410d480aca6b795f99
Chris Tankersley <chris@ctankersley.com>
2023-01-05 13:56:00 -0500

    fix(verify): Added missing remapping for params (#782)

--- packages/verify/lib/verify.ts
+++ packages/verify/lib/verify.ts
@@ -1,77 +1,120 @@
-import { Client } from '@vonage/server-client'
-import { Command } from './enums/Command'
-import { PSD2Request } from './interfaces/PSD2Request'
-import { VerifyCheckResponse } from './interfaces/Response/VerifyCheckResponse'
-import { VerifyControlResponse } from './interfaces/Response/VerifyControlResponse'
-import { VerifySearchResponse } from './interfaces/Response/VerifySearchResponse'
-import { VerificationRequest } from './interfaces/VerificationRequest'
+import { Client } from '@vonage/server-client';
+import { Command } from './enums/Command';
+import { PSD2Request } from './interfaces/PSD2Request';
+import { VerifyCheckResponse } from './interfaces/Response/VerifyCheckResponse';
+import { VerifyControlResponse } from './interfaces/Response/VerifyControlResponse';
+import { VerifySearchResponse } from './interfaces/Response/VerifySearchResponse';
+import { VerificationRequest } from './interfaces/VerificationRequest';
+
+const remapObjects = <T, O>(mapping, newObject: T, oldObject: O): T => {
+  for (const key in mapping) {
+    if (oldObject[mapping[key]]) {
+      newObject[key] = oldObject[mapping[key]];
+      delete oldObject[mapping[key]];
+    }
+  }
+  newObject = { ...newObject, ...oldObject };
+  return newObject;
+};
 
 export class Verify extends Client {
-    public async cancel(requestId: string): Promise<VerifyControlResponse> {
-        const data = {
-            request_id: requestId,
-            cmd: Command.CANCEL,
-        }
+  public async cancel(requestId: string): Promise<VerifyControlResponse> {
+    const data = {
+      request_id: requestId,
+      cmd: Command.CANCEL,
+    };
 
-        const resp = await this.sendPostRequest<VerifyControlResponse>(
-            `${this.config.apiHost}/verify/control/json`,
-            data
-        )
-        return resp.data
+    const resp = await this.sendPostRequest<VerifyControlResponse>(
+      `${this.config.apiHost}/verify/control/json`,
+      data,
+    );
+    return resp.data;
+  }
+
+  public async check(
+    requestId: string,
+    code: string,
+  ): Promise<VerifyCheckResponse> {
+    const data = {
+      request_id: requestId,
+      code,
+    };
+
+    const resp = await this.sendPostRequest<VerifyCheckResponse>(
+      `${this.config.apiHost}/verify/check/json`,
+      data,
+    );
+    return resp.data;
+  }
+
+  public async search(requestId: string): Promise<VerifySearchResponse> {
+    const resp = await this.sendGetRequest<VerifySearchResponse>(
+      `${this.config.apiHost}/verify/search/json`,
+      { request_id: requestId },
+    );
+
+    return resp.data;
+  }
+
+  public async start(
+    request: VerificationRequest | PSD2Request,
+  ): Promise<VerifyCheckResponse> {
+    let url = '';
+    if ('brand' in request) {
+      url = `${this.config.apiHost}/verify/json`;
+    } else {
+      url = `${this.config.apiHost}/verify/psd2/json`;
     }
 
-    public async check(
-        requestId: string,
-        code: string
-    ): Promise<VerifyCheckResponse> {
-        const data = {
-            request_id: requestId,
-            code,
-        }
-
-        const resp = await this.sendPostRequest<VerifyCheckResponse>(
-            `${this.config.apiHost}/verify/check/json`,
-            data
-        )
-        return resp.data
+    let newRequest: VerificationRequest | PSD2Request;
+    let mapping: any;
+    if ('brand' in request) {
+      newRequest = {
+        number: request.number,
+        brand: request.brand,
+      };
+      mapping = {
+        sender_id: 'senderId',
+        code_length: 'codeLength',
+        pin_expiry: 'pinExpiry',
+        lg: 'language',
+        next_event_wait: 'nextEventWait',
+        workflow_id: 'workflowId',
+      };
+    } else {
+      newRequest = {
+        number: request.number,
+        payee: request.payee,
+        amount: request.amount,
+      };
+      mapping = {
+        code_length: 'codeLength',
+        pin_expiry: 'pinExpiry',
+        lg: 'language',
+        next_event_wait: 'nextEventWait',
+        workflow_id: 'workflowId',
+      };
     }
 
-    public async search(requestId: string): Promise<VerifySearchResponse> {
-        const resp = await this.sendGetRequest<VerifySearchResponse>(
-            `${this.config.apiHost}/verify/search/json`,
-            { request_id: requestId }
-        )
+    newRequest = remapObjects(mapping, newRequest, request);
 
-        return resp.data
-    }
+    const resp = await this.sendPostRequest<VerifyCheckResponse>(
+      url,
+      newRequest,
+    );
+    return resp.data;
+  }
 
-    public async start(
-        request: VerificationRequest | PSD2Request
-    ): Promise<VerifyCheckResponse> {
-        let url = ''
-        if ('payee' in request) {
-            url = `${this.config.apiHost}/verify/psd2/json`
-        } else {
-            url = `${this.config.apiHost}/verify/json`
-        }
+  public async trigger(requestId: string) {
+    const data = {
+      request_id: requestId,
+      cmd: Command.TRIGGER_NEXT_EVENT,
+    };
 
-        const resp = await this.sendPostRequest<VerifyCheckResponse>(
-            url,
-            request
-        )
-        return resp.data
-    }
-
-    public async trigger(requestId: string) {
-        const data = {
-            request_id: requestId,
-            cmd: Command.TRIGGER_NEXT_EVENT,
-        }
-
-        const resp = await this.sendPostRequest<VerifyControlResponse>(
-            `${this.config.apiHost}/verify/control/json`,
-            data
-        )
-        return resp.data
-    }
+    const resp = await this.sendPostRequest<VerifyControlResponse>(
+      `${this.config.apiHost}/verify/control/json`,
+      data,
+    );
+    return resp.data;
+  }
 }
--- packages/verify/v2_TO_v3_MIGRATION_GUIDE.md
+++ packages/verify/v2_TO_v3_MIGRATION_GUIDE.md
@@ -9,36 +9,40 @@ If you would like to use this SDK as a standalone package, you can install it us
 ```console
 $ npm install @vonage/verify
 ```
+
 ## Setup
 
 ### With the Vonage Server SDK
+
 If you are using the main Vonage Node Server SDK, you will just need to configure it using a Vonage API Key and Secret. The messages client will then be available as `vonage.verify`.
 
 ```js
-const Vonage = require('@vonage/server-sdk');
+const Vonage = require('@vonage/server-sdk')
 
 const vonage = new Vonage({
     apiKey: API_KEY,
-    apiSecret: API_SECRET
-});
+    apiSecret: API_SECRET,
+})
 
-vonage.verify.check(VERIFY_REQUEST_ID, CODE)
-  .then(resp => console.log(resp))
-  .catch(err => console.error(err));
+vonage.verify
+    .check(VERIFY_REQUEST_ID, CODE)
+    .then((resp) => console.log(resp))
+    .catch((err) => console.error(err))
 ```
 
 ### Standalone
+
 This module is also capable of working as a standalone module. The only difference is that you create an `Verify` object and pass the credentials there.
 
 ```js
-const { Verify } = require('@vonage/verify');
+const { Verify } = require('@vonage/verify')
 ```
 
 Functionality-wise, the standalone version works just as the wrapped SDK version.
 
 ## Callbacks vs Promises
 
-The Node SDK v3.x uses Promises instead of callbacks. Customers will need to convert their callbacks to work with the responses returned from the calls. 
+The Node SDK v3.x uses Promises instead of callbacks. Customers will need to convert their callbacks to work with the responses returned from the calls.
 
 ## TypeScript
 
@@ -46,12 +50,12 @@ The module is fully written in TypeScript. While the users will be interacting w
 
 ## Changed Methods
 
-| 2.x Method | 3.x Method | Notes |
-|----------------|---------------|-------|
-| `vonage.verify.request()` | `vonage.verify.start()` | This method was renamed to better describe what it does, and now takes a `VerificationRequest` object or a `PSD2Request` object | 
-| `vonage.verify.psd2()` | - | This method was combined with `vonage.verify.start()` | 
-| `vonage.verify.check()` | `vonage.verify.check()` | This method now takes a request ID and code instead of various forms of input objects like a full Verification request | 
-| `vonage.verify.search()` | `vonage.verify.search()` | This method now searches for a Verification based on the request ID, and no longer accepts an array of multiple request IDs | 
-| `vonage.verify.control()` | - | This method has been broken up into discreet actions | 
-| - | `vonage.verify.cancel()` | Cancel the request verification request | 
-| - | `vonage.verify.trigger()` | Trigger the next event in the workflow | 
+| 2.x Method                | 3.x Method                | Notes                                                                                                                           |
+| ------------------------- | ------------------------- | ------------------------------------------------------------------------------------------------------------------------------- |
+| `vonage.verify.request()` | `vonage.verify.start()`   | This method was renamed to better describe what it does, and now takes a `VerificationRequest` object or a `PSD2Request` object |
+| `vonage.verify.psd2()`    | -                         | This method was combined with `vonage.verify.start()`                                                                           |
+| `vonage.verify.check()`   | `vonage.verify.check()`   | This method now takes a request ID and code instead of various forms of input objects like a full Verification request          |
+| `vonage.verify.search()`  | `vonage.verify.search()`  | This method now searches for a Verification based on the request ID, and no longer accepts an array of multiple request IDs     |
+| `vonage.verify.control()` | -                         | This method has been broken up into discreet actions                                                                            |
+| -                         | `vonage.verify.cancel()`  | Cancel the request verification request                                                                                         |
+| -                         | `vonage.verify.trigger()` | Trigger the next event in the workflow                                                                                          |
