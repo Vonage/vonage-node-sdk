e97f1e9722b8d7002a70964e0cb17a583cad1be2
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-01-06 18:32:49 +0000

    feat: object transformers on server client

---
+++ packages/server-client/lib/transformers.ts
@@ -0,0 +1,41 @@
+import camelCase from 'lodash.camelcase';
+import snakeCase from 'lodash.snakecase';
+import partial from 'lodash.partial';
+
+export const transformObjectKeys = (
+  transformFn: (key: string | number) => string,
+  objectToTransform: Record<string | number, unknown>,
+  deep = false,
+  preserve = false,
+): Record<string | number, unknown> => {
+  const transformedObject = {};
+
+  for (const prop in objectToTransform) {
+    if (!Object.hasOwn(objectToTransform, prop)) {
+      continue;
+    }
+
+    const value = objectToTransform[prop];
+    transformedObject[transformFn(prop)]
+            = deep
+            && typeof value === 'object'
+            && value !== null
+            && !Array.isArray(value)
+        ? transformObjectKeys(
+          transformFn,
+                      value as Record<string | number, unknown>,
+                      deep,
+                      preserve,
+        )
+        : value;
+  }
+
+  return {
+    ...transformedObject,
+    ...(preserve ? objectToTransform : {}),
+  };
+};
+
+export const camelCaseObjectKeys = partial(transformObjectKeys, camelCase);
+
+export const snakeCaseObjectKeys = partial(transformObjectKeys, snakeCase);
