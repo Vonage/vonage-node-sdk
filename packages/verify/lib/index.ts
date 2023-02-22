544f1e8d4f9ef64d49cc78b6d5243b63751aff33
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-02-22 14:34:18 +0000

    refactor: exporting types interfaces and enums

--- packages/verify/lib/index.ts
+++ packages/verify/lib/index.ts
@@ -1,3 +1,6 @@
-export { PSD2 } from './classes/PSD2'
-export { Verification } from './classes/Verification'
-export { Verify } from './verify'
+export { PSD2 } from './classes/PSD2';
+export { Verification } from './classes/Verification';
+export { Verify } from './verify';
+export * from './enums/index';
+export * from './interfaces/index';
+export * from './types/index';
