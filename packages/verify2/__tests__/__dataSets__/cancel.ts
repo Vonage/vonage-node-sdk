e177f4be6989e0914dfd2e769ec9eca87572d6d6
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-05-18 17:41:22 +0000

    feat(verify2): adding cancel method

---
+++ packages/verify2/__tests__/__dataSets__/cancel.ts
@@ -0,0 +1,28 @@
+export default [
+  {
+    label: 'cancel request',
+    request: ['/v2/verify/091e717f-8715-41a0-a3f0-cc04912deaa1', 'DELETE'],
+    response: [204],
+    method: 'post',
+    clientMethod: 'cancel',
+    parameters: ['091e717f-8715-41a0-a3f0-cc04912deaa1'],
+    expected: true,
+  },
+  {
+    label: 'error when request not found',
+    request: ['/v2/verify/091e717f-8715-41a0-a3f0-cc04912deaa1', 'DELETE'],
+    response: [
+      404,
+      {
+        title: 'Not Found',
+        type: 'https://developer.vonage.com/api-errors#not-found',
+        detail: 'Request <id> was not found or it has been verified already.',
+        instance: 'bf0ca0bf927b3b52e3cb03217e1a1ddf',
+      },
+    ],
+    method: 'delete',
+    clientMethod: 'cancel',
+    parameters: ['091e717f-8715-41a0-a3f0-cc04912deaa1'],
+    expected: false,
+  },
+];
