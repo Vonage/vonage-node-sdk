8dd49dde2f56c960a22f6d65f09eb607e50dd84a
Chuck Reeves <manchuck@users.noreply.github.com>
2023-08-09 11:26:30 -0400

    fix: type for websocket header (#851)

--- packages/voice/lib/types/Endpoint/WebsocketEndpoint.ts
+++ packages/voice/lib/types/Endpoint/WebsocketEndpoint.ts
@@ -2,5 +2,5 @@ export type WebsocketEndpoint = {
   type: 'websocket';
   uri: string;
   contentType?: string;
-  headers?: Array<Record<string, unknown>>;
+  headers?: Record<string, unknown>;
 };
