2b6447ae181826b6e5a9621bf6a78dce13c75105
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-11-02 15:25:21 +0000

    docs(proactive-connect): adding doc blocks

--- packages/proactive-connect/lib/types/event.ts
+++ packages/proactive-connect/lib/types/event.ts
@@ -1,12 +1,49 @@
-import { EventTypes } from '../enums/eventTypes';
+import { EventTypes } from '../enums';
 
+/**
+ * Represents an event that captures various activities or changes in the
+ * system, such as changes to user subscribe status, the creation of a new
+ * list based on a response, the invocation of a chat bot agent, or the
+ * triggering of certain analytics.
+ */
 export type Event = {
-    occurredAt: string
-    type: EventTypes
-    id: string
-    jobId: string
-    srcCtx: string
-    data: unknown
-    runId: string
-    recipientId: string
-}
+  /**
+   * The date and time when the event occurred.
+   */
+  occurredAt: string;
+
+  /**
+   * The type of the event, which should be one of the values from the 'EventTypes' enum.
+   */
+  type: EventTypes;
+
+  /**
+   * The unique identifier for the event.
+   */
+  id: string;
+
+  /**
+   * The unique identifier for the job related to the event.
+   */
+  jobId: string;
+
+  /**
+   * The name of the segment or matcher related to the event.
+   */
+  srcCtx: string;
+
+  /**
+   * The data associated with the event (unknown type).
+   */
+  data: unknown;
+
+  /**
+   * The unique identifier for the run related to the event.
+   */
+  runId: string;
+
+  /**
+   * The string identifier of a recipient, such as an email or phone number.
+   */
+  recipientId: string;
+};
--- packages/proactive-connect/lib/types/responses/listPageResponse.ts
+++ packages/proactive-connect/lib/types/responses/listPageResponse.ts
@@ -1,14 +1,46 @@
 import { APILinks } from '@vonage/server-client';
-import { SortOrder } from '../../enums/sortOrder';
+import { SortOrder } from '../../enums';
 import { ListResponse } from './listResponse';
 
+/**
+ * Represents a paginated response containing a list of lists with associated metadata.
+ * @remarks
+ * Vonage API's will return information using `snake_case`. This represents the
+ * pure response before the client will transform the keys into `camelCase`.
+ */
 export type ListPageResponse = {
-    page: number
-    page_size: number
-    total_items: number
-    total_pages: number
-    order: SortOrder
-    _embedded: {
-        lists: Array<ListResponse>
-    }
-} & APILinks
+  /**
+   * The page number of the current page.
+   */
+  page: number;
+
+  /**
+   * The number of lists per page.
+   */
+  page_size: number;
+
+  /**
+   * The total number of lists across all pages.
+   */
+  total_items: number;
+
+  /**
+   * The total number of pages.
+   */
+  total_pages: number;
+
+  /**
+   * The sorting order applied to the lists.
+   */
+  order: SortOrder;
+
+  /**
+   * An embedded object containing an array of list responses with associated metadata.
+   */
+  _embedded: {
+    /**
+     * An array of list responses with associated metadata.
+     */
+    lists: Array<ListResponse>;
+  };
+} & APILinks;
