08e4f34869b70dc7d2e10f10e4f7c0afbb885024
Chuck Reeves <manchuck@users.noreply.github.com>
2026-07-29 13:43:50 -0400

    feat: added unsupported network type for identity status (#1295)

--- packages/identity-insights/lib/enums/codeStatus.ts
+++ packages/identity-insights/lib/enums/codeStatus.ts
@@ -5,54 +5,59 @@ export enum Status {
   /**
    * The country or mobile network is not supported by available suppliers.
    */
-  NO_COVERAGE = 'no_coverage',
+  NO_COVERAGE = 'NO_COVERAGE',
 
   /**
    * The purpose used is not valid or allowed for this Insight.
    */
-  INVALID_PURPOSE = 'invalid_purpose',
+  INVALID_PURPOSE = 'INVALID_PURPOSE',
 
   /**
    * The request could not be authorized for the combination of application, supplier, and phone number.
    */
-  UNAUTHORIZED = 'unauthorized',
+  UNAUTHORIZED = 'UNAUTHORIZED',
 
   /**
    * An internal error occurred while processing the request.
    */
-  INTERNAL_ERROR = 'internal_error',
+  INTERNAL_ERROR = 'INTERNAL_ERROR',
 
   /**
    * The supplier returned an error while processing the request.
    */
-  SUPPLIER_ERROR = 'supplier_error',
+  SUPPLIER_ERROR = 'SUPPLIER_ERROR',
 
   /**
    * The phone number could not be found for this Insight.
    */
-  NOT_FOUND = 'not_found',
+  NOT_FOUND = 'NOT_FOUND',
 
   /**
    * The phone number format is not valid for assignment by carriers to users.
    */
-  INVALID_NUMBER_FORMAT = 'invalid_number_format',
+  INVALID_NUMBER_FORMAT = 'INVALID_NUMBER_FORMAT',
+
+  /**
+   * The network is not supported.
+   */
+  UNSUPPORTED_NETWORK_TYPE = 'UNSUPPORTED_NETWORK_TYPE',
 
   /**
    * Some response attributes were omitted because they are not applicable or were not available.
    */
-  PARTIAL_SUCCESS = 'partial_success',
+  PARTIAL_SUCCESS = 'PARTIAL_SUCCESS',
 
   /**
    * All Insight attributes are available and included in the response.
    */
-  OK = 'ok',
+  OK = 'OK',
 }
 
 export enum SubscriberMatchStatus {
   /**
    *  Operator requires idDocument to match any other attributes.
    */
-  SUBSCRIBER_MATCH_ID_DOCUMENT_REQUIRED = 'subscriber_match.id_document_required',
+  SUBSCRIBER_MATCH_ID_DOCUMENT_REQUIRED = 'SUBSCRIBER_MATCH.ID_DOCUMENT_REQUIRED',
 
   /**
    * Operator cannot match idDocument which is required to match any other attibutes.
